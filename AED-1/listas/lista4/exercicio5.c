fc88f1ea95e543a4a5aa20f0b965827f18ea7ea9
Olavo Vales <olavovgcs@gmail.com>
2024-04-28 22:00:51 -0300

    adição do código da questão 5

--- AED-1/listas/lista4/exercicio5.c
+++ AED-1/listas/lista4/exercicio5.c
@@ -2,31 +2,51 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void primo(int m, int *p1, int *p2){
+// Função para verificar se um número é primo
+int determinaPrimo(int n) {
 
-    setlocale(LC_ALL, "Portuguese");
-
-    while(*p1 < m){
-        *p1++;
-
-        printf("o maior numero primo menor que %d é %d, enquanto o menor numero primo maior que %d é %d", *p1, m, m, *p2);
+    // Verificar se n é divisível por algum número entre 2 e sua raiz quadrada
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return 0; // Não é primo se for divisível
     }
-
-
+    return 1; // Se não foi divisível, é primo
 }
 
-//exemplo: m = 12, maior numero primo menor que 12 -> 11 (*p1), menor numero primo maior que 12 -> 13(*p2)
+// Procedimento para encontrar o maior primo menor que m e o menor primo maior que m
+void primo(int m, int *p1, int *p2) {
+    // Encontrar o maior primo menor que m
+    for (int i = m - 1; i > 1; i--) { //diminuo até achar alguem numero que é primo
+        if (determinaPrimo(i)) {
+            *p1 = i;
+            break;
+        }
+    }
 
-int main (){ //função responsavel pelo input inicial e pela chamada do procedimento
+    // Encontrar o menor primo maior que m
+    for (int i = m + 1;; i++) {  //aumento até achar alguem numero que é primo
+        if (determinaPrimo(i)) {
+            *p2 = i;
+            break;
+        }
+    }
+}
+
+int main() {
 
     setlocale(LC_ALL, "Portuguese");
 
-    int numero1, numero2, numero3;
-
-    printf("digite o 1o número: ");
-    scanf("%d", &numero1);
-
-    primo(numero1, &numero2, &numero3);
-
+    int m, p1, p2;
+    
+    // Receber o valor de m
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &m);
+    
+    // Chamada do procedimento primo
+    primo(m, &p1, &p2);
+    
+    // Exibir os resultados
+    printf("O maior numero primo menor que %d é: %d\n", m, p1);
+    printf("O menor numero primo maior que %d é: %d\n", m, p2);
+    
     return 0;
 }
